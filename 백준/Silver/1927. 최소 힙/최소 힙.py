608eaf67e229b1c88540175f0e8d50165ee96985
Jintaek Jeong <87135698+jjt4515@users.noreply.github.com>
2024-10-19 23:09:01 +0900

    [Silver II] Title: 최소 힙, Time: 156 ms, Memory: 114136 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -5,10 +5,11 @@ n = int(stdin.readline())
 heap = []
 for _ in range(n):
     num = int(stdin.readline())
-    if num == 0:
-        if len(heap) == 0:
-            print(0)
-        else:
-            print(heapq.heappop(heap))
-    else:
+    if num != 0:
         heapq.heappush(heap, num)
+        
+    else:
+        try:
+            print(heapq.heappop(heap))
+        except:
+            print(0)
