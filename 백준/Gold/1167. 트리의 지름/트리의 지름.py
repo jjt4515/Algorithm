a48b0660a267ca8c357aaa94a68d1f2b05752e0b
Jintaek Jeong <87135698+jjt4515@users.noreply.github.com>
2024-10-26 23:57:25 +0900

    [Gold II] Title: 트리의 지름, Time: 3004 ms, Memory: 83468 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,25 +1,42 @@
-from sys import stdin
+# 트리는 다익스트라 이런거 필요 없다
+# 트리에서 임의의 한 점에서 가장 멀리 있는 노드를 찾고 그 노드에서 또 가장 멀리 있는 
+# 노드까지의 거리를 찾으면 트리의 지름이 된다.
 
-def dfs(x,y):
-    for a,b in tree[x]:
-        if visited[a] == -1:
-            visited[a] = b+y
-            dfs(a,b+y)
+import sys
+sys.setrecursionlimit(10**6)
 
-v = int(stdin.readline())
-tree = [[]for _ in range(v+1)]
-for i in range(v):
-    n = list(map(int,stdin.readline().split()))
-    for j in range(1,len(n)-2,2):
-        tree[n[0]].append([n[j],n[j+1]]) 
+v = int(input())
+tree = [[] for _ in range(v+1)]
 
-visited = [-1] * (v+1)
-visited[1] = 0
-dfs(1,0)
+for _ in range(v):
+    line = list(map(int, input().split()))
+    start = line[0]
+    i = 1
+    while line[i] != -1:
+        tree[start].append((line[i], line[i + 1]))
+        i += 2
 
-start = visited.index(max(visited))
+def dfs(start, cost):
+    visited[start] = cost
+
+    for n, d in tree[start]:
+        if visited[n] == -1:
+            dfs(n, d + cost)
+
+visited = [-1 for _ in range(v+1)]
+
+dfs(1, 0)
+
+temp = 0
+idx = 0
+for i in range(1, v+1):
+    if temp < visited[i]:
+        temp = visited[i]
+        idx = i
+
 visited = [-1]*(v+1)
-visited[start] = 0
-dfs(start,0)
+visited[idx] = 0
+dfs(idx, 0)
+
 
 print(max(visited))
