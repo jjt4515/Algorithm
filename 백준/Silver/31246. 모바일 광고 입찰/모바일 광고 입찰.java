815b3c7af674612282d23d158bcfb6621b5bf115
Jintaek Jeong <87135698+jjt4515@users.noreply.github.com>
2025-08-07 14:46:57 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 432 ms, Memory: 44996 KB -BaekjoonHub

---
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] moneyDif = new int[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            moneyDif[i] = b - a;
+        }
+
+        Arrays.sort(moneyDif);
+
+        System.out.println(Math.max(0, moneyDif[k - 1]));
+    }
+}
