bce521a814a6cc49b12a8328ae4c405c19789fdc
Jintaek Jeong <87135698+jjt4515@users.noreply.github.com>
2025-10-07 22:54:11 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 65.72 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,59 +1,62 @@
+import java.util.*;
 class Solution {
+    static boolean flag;
     public int[] solution(long[] numbers) {
-        int[] answer = new int[numbers.length];
-        for (int i=0;i<numbers.length;i++) {
-            String binary = toBin(numbers[i]);
-            int num = flagFunc(binary, 0, binary.length()-1);
-            if (num == -1)
-                answer[i] = 0;
-            else
+        int n = numbers.length;
+        int[] answer = new int[n];
+        for (int i=0;i<n;i++) {
+            long num = numbers[i];
+            ArrayList<Integer> al = new ArrayList<>();
+            while (num > 0) {
+                al.add((int)(num % 2));
+                num = num / 2;
+            }
+            
+            int p = 1;
+            while (true) {
+                int size = al.size();
+                if (size < Math.pow(2, p) - 1) {
+                    al.add(0);
+                } else if (size == Math.pow(2, p) - 1) {
+                    break;
+                } else {
+                    p++;
+                }
+            }
+            
+            Collections.reverse(al);
+
+            int size = al.size();
+            flag = true;
+            
+            if(search(al, 0, size-1) == 0) flag = false;
+            
+            if (flag) {
                 answer[i] = 1;
+            } else {
+                answer[i] = 0;
+            }
         }
         return answer;
     }
     
-    public String toBin(long num) {
-        StringBuilder sb = new StringBuilder();
-        while (num>0) {
-            sb.append(num % 2);
-            num /= 2;
-        }
-        int k = 1;
-        while (true) {
-            if (sb.length() > Math.pow(2,k)-1) {
-                k+=1;
-            } else {
-                break;
-            }
-        }
-        for (int i=0;i<Math.pow(2,k)-1 - sb.length();i++) {
-            sb.append('0');
-        }
-        sb.reverse();
-        return sb.toString();
-    }
-    
-    public int flagFunc(String s, int left, int right) {
+    public int search(ArrayList<Integer> al, int left, int right) {
         int mid = (left + right) / 2;
-        if (left >= right) {
-            if (s.charAt(mid) == '1')
-                return 1;
-            else 
-                return 0;
-        } 
-        int leftCheck = flagFunc(s, left, mid-1);
-        int rightCheck = flagFunc(s, mid+1, right);
-        if (leftCheck == -1 || rightCheck == -1)
-            return -1;
-        if (s.charAt(mid) == '1'){
-            return 1;
-        } else {
-            if (leftCheck == 0 && rightCheck == 0) 
-                return 0;
-            else
-                return -1;
-        }
-
         
+        if (left == right) {
+            return al.get(mid);
+        }
+        
+        int leftRes = search(al, left, mid-1);
+        int rightRes = search(al, mid+1, right);
+        
+        if (al.get(mid) == 1) 
+            return 1;
+
+        if (leftRes == 0 && rightRes == 0)
+            return 0;
+
+        flag = false;
+        return 0;
     }
 }
