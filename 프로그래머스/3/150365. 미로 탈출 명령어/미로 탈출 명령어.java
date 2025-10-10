a01899ab64bdd9176070185d761107f28d6bc9d5
Jintaek Jeong <87135698+jjt4515@users.noreply.github.com>
2025-10-10 20:41:11 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 5.70 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,40 +1,99 @@
+import java.util.*;
 class Solution {
-    static int N,M,R,C,K;
-    static int[] dx = {1, 0, 0, -1};
-    static int[] dy = {0, -1, 1, 0};
-    static String[] dir = {"d", "l", "r", "u"};
-    static String answer = "z";
+    static int[][] dis;
+    static int N, M, K, R, C;
+    static int[] dx = {1,0,0,-1}; //아래, 왼, 우, 위
+    static int[] dy = {0,-1,1,0};
+    static char[] answer;
+    static boolean flag = false;
+    static char[] dirs = {'d','l','r','u'};
+
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        dis = new int[n+1][m+1];
+        
+        answer = new char[k];
+        
         N = n;
         M = m;
+        K = k;
         R = r;
         C = c;
-        K = k;
-        if (distance(x,y) > k || (k - distance(x,y)) % 2 == 1)
+        
+        dis[r][c] = 0;
+
+        makeDis(r,c);
+
+        dfs(x,y,0);
+        
+        if (!flag)
             return "impossible";
-        dfs(x,y,0,"");
-        return answer;
+        
+        String ans = new String(answer);
+        return ans;
     }
     
-    public void dfs(int x, int y, int len, String route) {
-        if(len + distance(x, y) > K || route.compareTo(answer) > 0){
-            return;
-        }        
-        if (len == K && x == R && y == C) {
-            answer = route;
+    public void makeDis(int r, int c) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{r,c});
+        boolean[][] visited = new boolean[N+1][M+1];
+        visited[r][c] = true;
+        
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+            for (int i=0;i<4;i++) {
+                int nx = p[0] + dx[i];
+                int ny = p[1] + dy[i];
+                
+                if (overLine(nx,ny)) {
+                    continue;
+                }
+                
+                if (visited[nx][ny])
+                    continue;
+                
+                q.offer(new int[]{nx,ny});
+                visited[nx][ny] = true;
+                dis[nx][ny] = dis[p[0]][p[1]] + 1;
+            }
+        }
+        
+    }
+    
+    public void dfs(int x, int y, int depth) {
+        if (depth == K) {
+            if (x == R && y == C) {
+                flag = true;
+            }
             return;
         }
+        
         for (int i=0;i<4;i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if ((nx > 0 && nx <= N) && (ny > 0 && ny <= M)) {
-                dfs(nx,ny,len+1,route+dir[i]);
-            }
             
+            if (overLine(nx,ny))
+                continue;
+            
+            if (dis[nx][ny] > K - depth - 1)
+                continue;
+            
+            if ((K-depth-1 - dis[nx][ny]) % 2 == 1)
+                continue;
+            
+            answer[depth] = dirs[i];
+
+            dfs(nx,ny,depth+1);
+            
+            if (flag) 
+                break;
         }
     }
     
-    public int distance(int x, int y) {
-        return Math.abs(x-R) + Math.abs(y-C);
+    public boolean overLine(int r, int c) {
+        if (r <= 0 || r > N || c <= 0 || c > M)
+            return true;
+        return false;
     }
+
 }
+
