353715e2cc9ad462dfd0fd4b44d8375ceaa55aea
Jintaek Jeong <87135698+jjt4515@users.noreply.github.com>
2025-10-17 22:37:15 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,64 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[][] map;
+    static int[][][] dp;
+    static int N;
+    static int[] dx = {0,1,1};
+    static int[] dy = {1,0,1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N =  Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp = new int[N][N][3];
+        dp[0][1][0] = 1;
+        if (map[0][2] == 0) {
+            dp[0][2][0] = 1;
+        } else {
+            System.out.println(0);
+            return;
+        }
+
+        for (int i=3; i<N; i++) {
+            if (map[0][i] == 0 && dp[0][i-1][0] != 0)
+                dp[0][i][0] = 1;
+        }
+
+        for (int i=1;i<N;i++){
+            for (int j=2;j<N;j++){
+                if (map[i][j] == 1)
+                    continue;
+
+                dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+                dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+
+                if (map[i-1][j] == 1 || map[i][j-1] == 1)
+                    continue;
+
+                dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+            }
+        }
+        
+        System.out.println(dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2]);
+
+
+    }
+
+}
+
+
+
+
