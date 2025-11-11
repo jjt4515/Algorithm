89247fcda3cca0ad96d783ff5e05e178e6511541
Jintaek Jeong <87135698+jjt4515@users.noreply.github.com>
2025-11-11 16:53:45 +0900

    [Gold V] Title: A와 B, Time: 116 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,33 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder S = new StringBuilder(br.readLine());
+        StringBuilder T = new StringBuilder(br.readLine());
+
+        while (S.length() < T.length()) {
+            if (T.charAt(T.length() - 1) == 'A') {
+                T.deleteCharAt(T.length() - 1);
+
+            } else if (T.charAt(T.length() - 1) == 'B') {
+                T.deleteCharAt(T.length() - 1);
+                T.reverse();
+            }
+        }
+
+        System.out.println(T.toString().equals(S.toString()) ? 1 : 0);
+
+    }
+
+}
+
+
+
+
